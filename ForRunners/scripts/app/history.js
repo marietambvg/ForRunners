a85332545b6b47b412919619eb2b693e8876705c
Marieta Georgieva <marietambvg@gmail.com>
2013-10-13 18:33:20 -0500

    Changes made by Marieta Georgieva (marietambvg@gmail.com).

--- ForRunners/scripts/app/history.js
+++ ForRunners/scripts/app/history.js
@@ -1,30 +1,57 @@
-(function(global) {  
-    var HistoryViewModel,
-        app = global.app = global.app || {};
+/*(function(global) {  
+var HistoryViewModel,
+app = global.app = global.app || {};
     
-    HistoryViewModel = kendo.data.ObservableObject.extend({
-        historyDataSource: null,
-        historyIsSelectable:true,
+HistoryViewModel = kendo.data.ObservableObject.extend({
+historyDataSource: null,
+historyIsSelectable:true,
         
-        init: function () {
-            var that = this,
-                dataSource;
+init: function () {
+var that = this,
+dataSource;
             
-            kendo.data.ObservableObject.fn.init.apply(that, []);
+kendo.data.ObservableObject.fn.init.apply(that, []);
             
-            dataSource = window.localStorage.getItem("History");
-            if(dataSource){
-                dataSource=JSON.parse(dataSource);
-            } else{
-                dataSource=new Array();
-            }
+dataSource = window.localStorage.getItem("History");
+if(dataSource){
+dataSource=JSON.parse(dataSource);
+} else{
+dataSource=new Array();
+}
             
-            that.set("historyDataSource", dataSource);  
-            that.set("historyIsSelectable", true);
-        }        
-    });  
+that.set("historyDataSource", dataSource);  
+that.set("historyIsSelectable", true);
+}        
+});  
     
-    app.historyService = {
-        viewModel: new HistoryViewModel()
+app.historyService = {
+viewModel: new HistoryViewModel()
+};
+})(window);*/
+var app = app || {};
+
+(function(a) {
+    var viewModel = kendo.observable({
+        runs:[],
+        selectedRunData:[],
+        change:onCategoryChanged
+    });
+    
+    function init(e) {
+        kendo.bind(e.view.element, viewModel,kendo.mobile.ui);
+        //var data = (JSON.parse(window.localStorage.getItem("History")))[0].rundata;
+        var history = (JSON.parse(window.localStorage.getItem("History")));
+        
+        viewModel.set("runs", history);            
+    }
+    
+    function onCategoryChanged(e) {             
+        console.log(e.srcElement);
+        //var runData=e.sender._selectedValue;
+        //viewModel.set("selectedRunData", runData);
+    }
+    
+    a.runs = {
+        init:init          
     };
-})(window);
+}(app));
