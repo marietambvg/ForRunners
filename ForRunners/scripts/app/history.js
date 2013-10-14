0e705ee9c25245b7b46b2f0aa40d2a34353c3690
Marieta Georgieva <marietambvg@gmail.com>
2013-10-14 03:11:06 -0500

    Show details of the run route Changes made by Marieta Georgieva (marietambvg@gmail.com).

--- ForRunners/scripts/app/history.js
+++ ForRunners/scripts/app/history.js
@@ -1,56 +1,90 @@
-/*(function(global) {  
-var HistoryViewModel,
-app = global.app = global.app || {};
-    
-HistoryViewModel = kendo.data.ObservableObject.extend({
-historyDataSource: null,
-historyIsSelectable:true,
-        
-init: function () {
-var that = this,
-dataSource;
-            
-kendo.data.ObservableObject.fn.init.apply(that, []);
-            
-dataSource = window.localStorage.getItem("History");
-if(dataSource){
-dataSource=JSON.parse(dataSource);
-} else{
-dataSource=new Array();
-}
-            
-that.set("historyDataSource", dataSource);  
-that.set("historyIsSelectable", true);
-}        
-});  
-    
-app.historyService = {
-viewModel: new HistoryViewModel()
-};
-})(window);*/
 var app = app || {};
 
 (function(a) {
     var viewModel = kendo.observable({
         runs:[],
-        selectedRunData:[],
-        change:onCategoryChanged
+        selectedRun:null,
+        change:onRunChanged,
+        isInvisible:true,
+        isVisible:true,
+        isVisibleRunInfo:false
     });
     
+    var runsData;
+    
+    
     function init(e) {
-        kendo.bind(e.view.element, viewModel,kendo.mobile.ui);
-        //var data = (JSON.parse(window.localStorage.getItem("History")))[0].rundata;
-        var history = (JSON.parse(window.localStorage.getItem("History")));
+        kendo.bind(e.view.element, viewModel);
         
-        viewModel.set("runs", history);            
+        var savedRuns = window.localStorage.getItem("History");
+        if (savedRuns != null) {
+            runsData=[];
+            runsData=JSON.parse(savedRuns);
+            viewModel.set("runs", runsData); 
+            viewModel.set("isVisible", false); 
+            viewModel.set("isInvisible", false);
+            viewModel.set("isVisibleRunInfo", false);
+        }
     }
     
-    function onCategoryChanged(e) {             
-        console.log(e.srcElement);
-        //var runData=e.sender._selectedValue;
-        //viewModel.set("selectedRunData", runData);
+    function onRunChanged(e) {
+        viewModel.set("isVisibleRunInfo", true);
+        var returnedValue;
+        returnedValue=(e.sender._selectedValue);
+        console.log((returnedValue));
+        var currentRundata=[];
+        var runNumber=0;
+        for (var i = 0; i < runsData.length; i++) {
+            if(runsData[i].runname==returnedValue){
+                currentRundata=runsData[i].rundata;
+                runNumber=i;
+                break;
+            }
+        }
+        console.log(currentRundata[0].lat);
+        
+        viewModel.set("runDate", runsData[runNumber].runname);
+        viewModel.set("runDistance", runsData[runNumber].rundistance);
+        viewModel.set("runTime", runsData[runNumber].runtime);
+        viewModel.set("runSpeed", runsData[runNumber].runspeed);
+        trackRoute(currentRundata);
     }
     
+    function trackRoute(currentRundata){
+        
+        var data = currentRundata;
+                var myLatLng = new google.maps.LatLng(data[0].lat, data[0].lon);
+
+                // Google Map options
+                var myOptions = {
+                    zoom: 15,
+                    center: myLatLng,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                // Create the Google Map, set options
+                var map = new google.maps.Map(document.getElementById("route-holder"), myOptions);
+
+                var trackCoords = [];
+    
+                // Add each GPS entry to an array
+                for (i=0; i < data.length; i++) {
+                    trackCoords.push(new google.maps.LatLng(data[i].lat, data[i].lon));
+                }
+    
+                // Plot the GPS entries as a line on the Google Map
+                var trackPath = new google.maps.Polyline({
+                    path: trackCoords,
+                    strokeColor: "#FF0000",
+                    strokeOpacity: 1.0,
+                    strokeWeight: 2
+                });
+
+                // Apply the line to the map
+                trackPath.setMap(map);
+                //app.application.navigate("views/route-view.html#route-view")
+        
+    }
     a.runs = {
         init:init          
     };
